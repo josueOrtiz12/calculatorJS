0cf15937ed75c304006a8a36d3326a9e239fcfd7
josue ortiz <ortizjd0036@gmail.com>
2024-03-03 14:42:53 -0400

    feat: finish calculator 1.0

--- js/main.js
+++ js/main.js
@@ -3,17 +3,41 @@ const buttons = document.querySelectorAll(".btn");
 
 // console.log(buttons);
 
-
 for(button of buttons){
     button.addEventListener("click" , function(){
         const buttonSelection = this.textContent;
-        // console.log(valorContent);
-        if(screen.value == 0){
+        console.log(buttonSelection);
+
+        if(this.id === "keyC"){
+            screen.value = 0;
+            return;
+        };
+
+        if(this.id === "delete"){
+            if(screen.value.length === 1 || screen.value === "!Error" ){
+                screen.value = "0"
+            }else{
+                const actualValue = screen.value;
+                const newValue = actualValue.slice(0 , -1);
+                screen.value = newValue;
+            }
+           return
+        };
+        if(this.id === "equal"){
+            try {
+                screen.value = eval(screen.value); 
+            } catch {
+                    screen.value = "!Error";
+            }
+       
+            return;
+        }
+
+        if(screen.value === "0" || screen.value === "!Error"){
             screen.value = buttonSelection
         }else{
             screen.value += buttonSelection;
-        }
-       
-    })
+        } ;   
+    });
 }
 
